470ad45742a766fa780ba02032a3ab639d41b15c
jum0624 <jum0624@naver.com>
2022-07-13 03:15:32 +0900

    build: MemoryDB build

---
+++ practice/memoryDB/src/main/java/test/memoryDB/MemoryDbApplication.java
@@ -0,0 +1,13 @@
+package test.memoryDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryDbApplication.class, args);
+	}
+
+}
