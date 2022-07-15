918a4dd1bf797d202b8d14cd7b6ae59d96f6cd1f
jum0624 <jum0624@naver.com>
2022-07-15 14:18:36 +0900

    chore: jdbc repository삭제에 따른 config 수정

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,8 +1,6 @@
 package hello.hellospring;
 
 import hello.hellospring.Service.MemberService;
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +11,6 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;  // dataSource 는 데이터베이스 커넥션을 획득할 때 사용하는 객체.
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -25,8 +18,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new MemoryMemberRepository();
     }
 }
