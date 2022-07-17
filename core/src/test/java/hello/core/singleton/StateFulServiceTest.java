194e8ffc192a0052fa95384f017512b1eb11e710
jum0624 <jum0624@naver.com>
2022-07-17 18:07:20 +0900

    Test: 싱글톤 멀티 스레드 문제 지역변수 선언으로 해결

--- core/src/test/java/hello/core/singleton/StateFulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StateFulServiceTest.java
@@ -18,15 +18,15 @@ class StateFulServiceTest {
         StateFulService stateFulService2 = ac.getBean(StateFulService.class);
 
         //ThreadA: A사용자 10000원 주문
-        stateFulService1.order("userA", 10000);
+        int userAPrice = stateFulService1.order("userA", 10000);
         //ThreadB: B사용자 20000원 주문
-        stateFulService2.order("userB", 20000);
+        int userBPrice = stateFulService2.order("userB", 20000);
 
         //ThreadA: 사용자A 주문 금액 조회
-        int price = stateFulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = stateFulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        assertThat(stateFulService1.getPrice()).isEqualTo(20000);
+        assertThat(userBPrice).isEqualTo(20000);
     }
 
     static class TestConfig {
