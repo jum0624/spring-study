2c5667a699a08511dabcd8c7e9b1f962c9599444
jum0624 <jum0624@naver.com>
2022-07-18 01:43:04 +0900

    Test: @Configuration과 싱글톤

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,40 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {  // 역할들이 드러나는 것이 중요!!
 
+
+    // memberService -> new MemoryMemberRepository
+    // orderService -> new MemoryMemberRepository
+    // 두개의 객체가 생성 되면서 싱글톤이 아닌것인가?
+
+    // 예상 출력
+//    call AppConfig.memberService
+//    call AppConfig.MemberRepository
+//    call AppConfig.MemberRepository
+//    call AppConfig.orderService
+//    call AppConfig.MemberRepository
+
+    // 실제 출력
+//    call AppConfig.memberService
+//    call AppConfig.MemberRepository
+//    call AppConfig.orderService
+// memberRepository가 세번 호출 되어야 하는데 한번만 호출됨 -> 스프링이 싱글톤을 보장해주고 있음을 볼 수 있음
+
+
     @Bean
     public MemberService memberService() {  // 멤버 서비스의 역할
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(MemberRepository());  // 생성자 주입
     }
 
     @Bean
     public MemberRepository MemberRepository() {  // 멤버 레포지토리의 역할
+        System.out.println("call AppConfig.MemberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){  // 주문 서비스의 역할
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
@@ -23,5 +24,10 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // test 용
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
