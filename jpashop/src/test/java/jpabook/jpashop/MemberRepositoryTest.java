7323365dcf4362bb43b0aa0dfb3be2c883ce4183
jum0624 <jum0624@naver.com>
2022-08-06 20:10:46 +0900

    fix: MemberRepositoryTest JUnit4로 변경

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,31 +8,28 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Rollback(value = false)
-class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
-    public void testMember() throws Exception {
-
-        //given
+    @Rollback(false)
+    public void testMember() {
+        // given
         Member member = new Member();
         member.setUsername("memberA");
 
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
+        // when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
 
         //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member = " + (findMember == member));
-
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());  // JPA 엔티티 동일성 보장
     }
-
 }
