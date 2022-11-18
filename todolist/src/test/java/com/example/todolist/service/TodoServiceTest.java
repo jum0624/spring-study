227fdb4e84634dce1796b848fe2bda17af424e42
jum0624 <jum0624@naver.com>
2022-11-19 03:53:17 +0900

    Test: Todo Service Test code

---
+++ todolist/src/test/java/com/example/todolist/service/TodoServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.todolist.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void searchAll() {
+    }
+
+    @Test
+    void searchById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
